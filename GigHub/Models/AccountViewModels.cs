f8ce971a23ead5e3b7ef6489b8e424c204374912
Gerhard <gerhardengel@gmail.com>
2020-09-30 13:52:45 +0200

    Added Name to registration

--- GigHub/Models/AccountViewModels.cs
+++ GigHub/Models/AccountViewModels.cs
@@ -65,6 +65,11 @@ namespace GigHub.Models
     public class RegisterViewModel
     {
         [Required]
+        [Display(Name = "Name")]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
