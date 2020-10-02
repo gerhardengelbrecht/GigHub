0463991159c6dbaf389cc459d7500141155d3e94
Gerhard <gerhardengel@gmail.com>
2020-10-02 05:43:08 +0200

    Gig Notification Refactor

--- GigHub/Controllers/Api/GigsController.cs
+++ GigHub/Controllers/Api/GigsController.cs
@@ -1,6 +1,6 @@
 ﻿using GigHub.Models;
 using Microsoft.AspNet.Identity;
-using System;
+using System.Data.Entity;
 using System.Linq;
 using System.Web.Http;
 
@@ -20,7 +20,10 @@ namespace GigHub.Controllers.Api
         public IHttpActionResult Cancel(int id)
         {
             var userId = User.Identity.GetUserId();
-            var gig = _context.Gigs.Single(g => g.Id == id && g.ArtistId == userId);
+            var gig = _context
+                .Gigs
+                .Include(g => g.Attendances.Select(a => a.Attendee))
+                .Single(g => g.Id == id && g.ArtistId == userId);
 
             if (gig.IsCanceled)
             {
@@ -29,23 +32,11 @@ namespace GigHub.Controllers.Api
 
             gig.IsCanceled = true;
 
-            var notification = new Notification
-            {
-                DateTime = DateTime.Now,
-                Gig = gig,
-                Type = NotificationType.GigCanceled
-            };
+            var notification = new Notification(NotificationType.GigCanceled, gig);
 
-            var attendees = _context.Attendances.Where(a => a.GigId == gig.Id).Select(a => a.Attendee).ToList();
-
-            foreach (var attendee in attendees)
+            foreach (var attendee in gig.Attendances.Select(a => a.Attendee))
             {
-                var userNotification = new UserNotification
-                {
-                    User = attendee,
-                    Notification = notification
-                };
-                _context.UserNotifications.Add(userNotification);
+                attendee.Notify(notification);
             }
 
 
