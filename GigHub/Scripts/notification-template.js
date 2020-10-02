51e4aba0ae1ae4e4cb8a4bd07b04559f40901774
Gerhard <gerhardengel@gmail.com>
2020-10-02 11:59:08 +0200

    Notification popover styling

--- GigHub/Scripts/notification-template.js
+++ GigHub/Scripts/notification-template.js
@@ -1,14 +1,9 @@
 ﻿//<%
 _.each(notifications,
     function(notification) {
-        if (notification.type === 0) {
-            //%>
-            //<li>No new notifications</li>
-            //<%
-        }
         if (notification.type === 1) {
             //%>
-            //<li><%= notification.gig.artist.name %>has canceled the gig at <%= notification.gig.venue %> as <%= notification.gig.datetime %></li>
+            //<li><span class="highlight"><%= notification.gig.artist.name %></span> has canceled the gig at <%= notification.gig.venue %> as <%= moment(notification.gig.datetime).format("D MMM HH:mm") %></li>
             //<%
         } else if (notification.type === 2) {
             var changes = [],
@@ -23,11 +18,11 @@ _.each(notifications,
 
             if (notification.orginalDateTime !== notification.gigId.dateTime) {
                 changes.push('date/time');
-                originalValues.push(notification.originalDateTime);
-                newValues.push(notification.gig.dateTime);
+                originalValues.push(moment(notification.originalDateTime).format("D MMM HH:mm"));
+                newValues.push(moment(notification.gig.dateTime).format("D MMM HH:mm"));
             }
             //%>
-            //<li <% notification.gig.artist.name %> has changed the <%= changes.join(' and ') of the gig from <%= originalValues %> to <%= newValues %> </li> 
+            //<li <span class="highlight"><% notification.gig.artist.name %></span> has changed the <%= changes.join(' and ') of the gig from <%= originalValues.join('/') %> to <%= newValues.join('/') %> </li> 
             //%>
         }
     });
